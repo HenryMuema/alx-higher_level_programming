bdbcec8ccf84bb2258931124f4dc7512b143858d
HenryMuema <wambuamuemaa@gmail.com>
2023-01-25 16:19:22 +0000

    tasksss

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -2,10 +2,17 @@
 class Square:
 	def __init__(_Square, size=0):
 		_Square.__size = size
-
+	
 		if not isinstance(_Square.__size, int):	
 			raise TypeError("size must be an integer")
 		elif _Square.__size < 0:
 			raise ValueError("size must be >= 0")
 	def area(self):
 		return self.__size * self.__size
+	def size(self):
+		return self.__size
+	def size(self,value):
+		if not isinstance(self.__value, int):
+			raise TypeError("size must be an integer")
+		elif self.__value < 0:
+			raise ValueError("size must be >= 0")
